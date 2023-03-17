e29eafa0a4845c1f60a20d5e6adc121d5ac19b87
YuthanaRodjanasom <yuthana_r@kkumail.com>
2023-03-17 16:20:28 +0700

    [Updated]:re export along modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
-export { default } from "./compoents/AlongProvider"
-export { default as AlongAction } from "./atom/AlongAction"
+export {default} from './compoents/AlongProvider';
+export {default as AlongAction} from './atom/AlongAction';
+export {default as AlongModal} from './compoents/AlongModal';
