e519b7faa7b4e6423bbc89b4f30efd1b06733c8e
YuthanaRodjanasom <yuthana_r@kkumail.com>
2023-03-20 10:26:28 +0700

    [Updated]:along modal rollback

--- index.tsx
+++
@@ -1,3 +0,0 @@
-export {default} from './src/components/AlongProvider';
-export {default as AlongAction} from './src/atom/AlongAction';
-export {default as AlongModal} from './src/components/AlongModal';
--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export {default} from './components/AlongProvider';
+export {default as AlongModal} from './components/AlongModal';
 export {default as AlongAction} from './atom/AlongAction';
